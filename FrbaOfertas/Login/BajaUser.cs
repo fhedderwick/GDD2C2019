0e76958f3e124cbb809c23cdedbe5fdf7d6e31bb
nespada <nnmmee.95@gmail.com>
2019-11-30 12:20:55 -0300

    Cambios User Baja, detalles.

--- FrbaOfertas/Login/BajaUser.cs
+++ FrbaOfertas/Login/BajaUser.cs
@@ -16,6 +16,7 @@ namespace FrbaOfertas.Login
         private string queryUser = "SELECT USER_USERNAME FROM MANA.USUARIO WHERE USER_USERNAME = @Username";
         private string queryPassword = "SELECT USER_PASSWORD FROM MANA.USUARIO WHERE USER_USERNAME = @Username";
         private string queryUserId = "SELECT USER_ID FROM MANA.USUARIO WHERE USER_USERNAME = @Username";
+        private string queryEstado = "SELECT USUARIO_ESTADO FROM MANA.USUARIO WHERE USER_USERNAME = @Username";
 
         public BajaUser(DataBaseManager dbm)
         {
@@ -37,23 +38,30 @@ namespace FrbaOfertas.Login
                 string password = t2.Text;                
                  Dictionary<string, object> map = new Dictionary<string, object>();
                         map.Add("@Username", username);
-                 Dictionary<string, object> mapp = new Dictionary<string, object>();
-                        mapp.Add("@Password", password);
-                        _dbm.executeProcedure("Mana.ValidarPassword", mapp);
+                 Dictionary<string, object> map2 = new Dictionary<string, object>();
+                        map2.Add("@Password", password);
+                        _dbm.executeProcedure("Mana.ValidarPassword", map2);
                         password = _dbm.executeSelectString("SELECT USER_PASSWORD FROM MANA.TT");
                                                            //Valido que el usuario Exista y que la contraseña actual ingresada sea correcta
                         if (_dbm.executeSelectString(queryUser, map) == username && _dbm.executeSelectString(queryPassword, map) == password)
                         {
-                            Dictionary<string, object> ma = new Dictionary<string, object>();
-                            ma.Add("@Username", username);
-                            int userId = _dbm.executeSelectInt(queryUserId, ma);
-                            Dictionary<string, object> m = new Dictionary<string, object>();
-                            m.Add("@UserId", userId.ToString());                            
-                            _dbm.executeProcedure("MANA.BajaUsuario", m);
+                            Dictionary<string, object> map3 = new Dictionary<string, object>();
+                             map3.Add("@Username", username);
+                             string estado = _dbm.executeSelectString(queryEstado, map3);
+                             if (estado != "Deshabilitado") //Valido que el usuario no se encuentre deshabilitado
+                             {
+                                 Dictionary<string, object> map4 = new Dictionary<string, object>();
+                                 map4.Add("@Username", username);
+                                 int userId = _dbm.executeSelectInt(queryUserId, map4);
+                                 Dictionary<string, object> map5 = new Dictionary<string, object>();
+                                 map5.Add("@UserId", userId.ToString());
+                                 _dbm.executeProcedure("MANA.BajaUsuario", map5);
 
-                            MessageBox.Show("La operacion se ha realizado con exito", "Exito", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                            Hide();
-                            this.Close();
+                                 MessageBox.Show("La operacion se ha realizado con exito", "Exito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                 Hide();
+                                 this.Close();
+                             }
+                             else { MessageBox.Show("El usuario ya se encuentra Deshabilitado.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning); }                            
                         }
                         else { MessageBox.Show("El usuario o la contraseña ingresada no son validos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning); }
                               _dbm.executeProcedure("Mana.BorrarTT");
